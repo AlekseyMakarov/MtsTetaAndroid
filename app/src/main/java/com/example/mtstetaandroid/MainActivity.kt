12eb52391cb8140b0e559beb48e1c9fe6e31fe43
Aleksey Makarov <leha199600@gmail.com>
2021-07-23 18:23:50 +0300

    added click listener

--- app/src/main/java/com/example/mtstetaandroid/MainActivity.kt
+++ app/src/main/java/com/example/mtstetaandroid/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.mtstetaandroid
 
+import android.graphics.Rect
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.LayerDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RatingBar
+import android.widget.Toast
 import androidx.core.graphics.drawable.toBitmap
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mtstetaandroid.data.dto.MovieDto
+import ru.mts.teta.summer.android.homework.list.data.features.movies.MoviesDataSourceImpl
 
 class MainActivity : AppCompatActivity() {
 	private lateinit var moviesModel: MoviesModel
@@ -13,40 +20,49 @@ class MainActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.movie_item)
-		val rating = findViewById<RatingBar>(R.id.rb_rating)
-		val starEmptyBitmap = getDrawable(R.drawable.ic_star_empty)?.toBitmap(20, 20)
-		val starBitmap = getDrawable(R.drawable.ic_star)?.toBitmap(20, 20)
-		val newStarEmptyDrawable = BitmapDrawable(resources, starEmptyBitmap)
-		val newStarDrawable = BitmapDrawable(resources, starBitmap)
+		setContentView(R.layout.movies_list)
 
-		val finalDrawable = LayerDrawable(
-			arrayOf(
-				newStarEmptyDrawable,
-				newStarEmptyDrawable,
-				newStarDrawable,
-			)
-		)
-		finalDrawable.setId(0, android.R.id.background)
-		finalDrawable.setId(1, android.R.id.secondaryProgress)
-		finalDrawable.setId(2, android.R.id.progress)
-		rating.setProgressDrawableTiled(finalDrawable)
-//		val recycler = findViewById<RecyclerView>(R.id.recyclerViewGenres)
-//		val genres = listOf("боевик", "драма", "мелодрама", "ужасы", "триллеры", "комедии", "хаус", "про войну", "документальные")
-//		val adapter = GenreAdapter()
-//		adapter.setData(genres)
-//		recycler.adapter = adapter
-//		recycler.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+		val recycler = findViewById<RecyclerView>(R.id.recyclerViewGenres)
+		val genres = listOf("боевик", "драма", "мелодрама", "ужасы", "триллеры", "комедии", "хаус", "про войну", "документальные")
+		val adapter = GenreAdapter()
+		adapter.setData(genres)
+		recycler.adapter = adapter
+		recycler.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+
+
+		val recyclerMovie = findViewById<RecyclerView>(R.id.recyclerViewMovies)
+		initDataSource()
+
+		val movies = moviesModel.getMovies()
+		val adapterMovie = MovieAdapter()
+		adapterMovie.setOnClickListener (::showToast)
+		adapterMovie.setData(movies)
+		recyclerMovie.addItemDecoration(object : RecyclerView.ItemDecoration() {
+			override fun getItemOffsets(outRect: Rect, itemPosition: Int, parent: RecyclerView) {
+				outRect.set(
+					if (itemPosition % 2 == 0) dpToPx(20f) else dpToPx(10f),
+					if (itemPosition < 2) dpToPx(0f) else dpToPx(50f),
+					if (itemPosition % 2 == 1) dpToPx(20f) else dpToPx(10f),
+					when (parent.adapter!!.itemCount%2){
+						0 -> if (itemPosition == parent.adapter!!.itemCount - 1 || itemPosition == parent.adapter!!.itemCount - 2) dpToPx(16f) else 0
+						1-> if (itemPosition == parent.adapter!!.itemCount - 1) dpToPx(16f) else 0
+						else -> 0
+					}
+				)
+			}
+		})
+		recyclerMovie.adapter = adapterMovie
+		recyclerMovie.layoutManager = GridLayoutManager(this, 2, RecyclerView.VERTICAL, false)
 		
-//		initDataSource()
+
 //		setupViews()
 //		setupListeners()
-	}}
+	}
 	
-//	private fun initDataSource() {
-//		moviesModel = MoviesModel(MoviesDataSourceImpl())
-//	}
-//
+	private fun initDataSource() {
+		moviesModel = MoviesModel(MoviesDataSourceImpl())
+	}
+
 //	private fun setupViews() {
 //		getMovieAt(MOVIES_INITIAL_POSITION)?.let {
 //			// https://coil-kt.github.io/coil/
@@ -60,7 +76,7 @@ class MainActivity : AppCompatActivity() {
 //				getString(R.string.main_age_restriction_text, it.ageRestriction)
 //		}
 //	}
-//
+
 //	private fun setupListeners() {
 //		findViewById<View>(R.id.root)?.setOnClickListener {
 //			showToast(getToastMessage())
@@ -80,12 +96,12 @@ class MainActivity : AppCompatActivity() {
 //		getString(R.string.main_click_message, it)
 //	}
 //
-//	private fun showToast(message: String?) {
-//		when {
-//			message.isNullOrEmpty() -> { showToast(getString(R.string.main_empty_message)) }
-//			else -> Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-//		}
-//	}
-//}
+	fun showToast(message: String?) {
+		when {
+			message.isNullOrEmpty() -> { showToast(getString(R.string.main_empty_message)) }
+			else -> Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+		}
+	}
+}
 //
 //private const val MOVIES_INITIAL_POSITION = 0
