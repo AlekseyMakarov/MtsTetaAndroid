2d78e82e2986d202e1039191dbcdf4c611a5a876
Aleksey Makarov <leha199600@gmail.com>
2021-07-28 19:34:14 +0300

    develop in process3

--- app/src/main/java/com/example/mtstetaandroid/ui/movieDetails/DetailsFragment.kt
+++ app/src/main/java/com/example/mtstetaandroid/ui/movieDetails/DetailsFragment.kt
@@ -1,22 +1,51 @@
 package com.example.mtstetaandroid.ui.movieDetails
 
+import android.content.Context
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.LayerDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import coil.load
 import com.example.mtstetaandroid.R
 import com.example.mtstetaandroid.data.dto.MovieDto
+import com.example.mtstetaandroid.dpToPx
 import com.example.mtstetaandroid.ui.dashboard.DashboardViewModel
 
 class DetailsFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
+    private var scaledRatingDrawable: Drawable? = null
+
+
+    private fun createScaledRatingDrawable(context: Context): Drawable {
+
+        val starEmptyBitmap = AppCompatResources.getDrawable(context, R.drawable.ic_star_empty)?.toBitmap(context.dpToPx(15F), context.dpToPx(15F))
+        val starBitmap = AppCompatResources.getDrawable(context, R.drawable.ic_star)?.toBitmap(context.dpToPx(15F),context.dpToPx(15F))
+        val newStarEmptyDrawable = BitmapDrawable(context.resources, starEmptyBitmap)
+        val newStarDrawable = BitmapDrawable(context.resources, starBitmap)
+        val finalDrawable = LayerDrawable(
+            arrayOf(
+                newStarEmptyDrawable,
+                newStarEmptyDrawable,
+                newStarDrawable,
+            )
+        )
+        finalDrawable.setId(0, android.R.id.background)
+        finalDrawable.setId(1, android.R.id.secondaryProgress)
+        finalDrawable.setId(2, android.R.id.progress)
+        return finalDrawable
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -30,6 +59,13 @@ class DetailsFragment : Fragment() {
 //        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
 //            textView.text = it
 //        })
+        if (scaledRatingDrawable == null) {
+            scaledRatingDrawable = createScaledRatingDrawable(root.context)
+        }
+
+
+
+
         val imageView = root.findViewById<ImageView>(R.id.imageView)?.apply {
            this.load(arguments?.getString("movie_image_url"))
         }
@@ -41,6 +77,10 @@ class DetailsFragment : Fragment() {
         }
         val textViewTitle = root.findViewById<TextView>(R.id.textViewFilmName)?.apply {
             text = arguments?.getString("movie_title")
+        }
+        val ratingBar = root.findViewById<RatingBar>(R.id.ratingBarMovieRatingDetails).apply {
+            setProgressDrawableTiled(scaledRatingDrawable)
+            rating = arguments?.getInt("movie_rate_score")!!.toFloat()
         }
 //        val textViewRateScore = root.findViewById<TextView>(R.id.textViewFilmName)?.apply {
 //            text = arguments?.getString("movie_title")
