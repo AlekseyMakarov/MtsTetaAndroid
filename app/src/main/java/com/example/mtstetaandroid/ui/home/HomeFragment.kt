5b407a9abc1426ffd96dc4da09b2213568e071c0
Aleksey Makarov <leha199600@gmail.com>
2021-08-04 23:12:47 +0300

    added MovieViewModel

--- app/src/main/java/com/example/mtstetaandroid/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mtstetaandroid/ui/home/HomeFragment.kt
@@ -1,7 +1,7 @@
 package com.example.mtstetaandroid.ui.home
 
-import android.graphics.Rect
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,13 +13,18 @@ import com.example.mtstetaandroid.*
 import com.example.mtstetaandroid.data.GenresModel
 import com.example.mtstetaandroid.data.dto.MovieDto
 import ru.mts.teta.summer.android.homework.list.data.features.movies.GenresDataSourceImpl
-import ru.mts.teta.summer.android.homework.list.data.features.movies.MoviesDataSourceImpl
-import com.example.mtstetaandroid.extensions.dpToPx
 import com.example.mtstetaandroid.ui.movieDetails.DetailsFragment
+import androidx.fragment.app.activityViewModels
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+
+
+object HomeFragmentConstants {
+    const val FRAGMENT_NAME = "HomeFragment"
+}
 
 class HomeFragment : Fragment() {
 
-    private lateinit var moviesModel: MoviesModel
+    val viewModel by activityViewModels<MovieViewModel>()
     private lateinit var genresModel: GenresModel
 
     override fun onCreateView(
@@ -27,7 +32,15 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(root: View, savedInstanceState: Bundle?) {
+        val v: SwipeRefreshLayout =
+            root.findViewById<SwipeRefreshLayout>(R.id.swipeRefreshLayoutHome)
+        v.setOnRefreshListener {
+            viewModel.refresh()
+        }
         val recycler = root.findViewById<RecyclerView>(R.id.recyclerViewGenres)
         initGenresSource()
         val genres = genresModel.getGenres()
@@ -36,21 +49,30 @@ class HomeFragment : Fragment() {
         recycler.adapter = adapter
         recycler.layoutManager = LinearLayoutManager(root.context, RecyclerView.HORIZONTAL, false)
         val recyclerMovie = root.findViewById<RecyclerView>(R.id.recyclerViewMovies)
-        initDataSource()
-        val movies = moviesModel.getMovies()
         val adapterMovie = MovieAdapter()
         adapterMovie.setOnClickListener(::showDetails)
-        adapterMovie.setData(movies)
+
         recyclerMovie.addItemDecoration(RecyclerDecoration)
         recyclerMovie.adapter = adapterMovie
         recyclerMovie.layoutManager =
             GridLayoutManager(root.context, 2, RecyclerView.VERTICAL, false)
-        return root
-    }
 
-
-    private fun initDataSource() {
-        moviesModel = MoviesModel(MoviesDataSourceImpl())
+        viewModel.movies.observe(viewLifecycleOwner) { state: MoviesState ->
+            when (state) {
+                MoviesState.ErrorState -> {
+                    v.isRefreshing = false
+                    Log.e(HomeFragmentConstants.FRAGMENT_NAME, "error occured while loading")
+                }
+                MoviesState.LoadingState -> {
+                    v.isRefreshing = true
+                    Log.i(HomeFragmentConstants.FRAGMENT_NAME, "loading")
+                }
+                is MoviesState.SuccessState -> {
+                    v.isRefreshing = false
+                    adapterMovie.setData(state.movies)
+                }
+            }
+        }
     }
 
     private fun initGenresSource() {
